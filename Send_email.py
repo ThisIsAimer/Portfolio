7038bcbc9bb93a935d09cae8a0e2031ef58f126c
raunak <raunakbiswas05@gmail.com>
2024-12-03 18:19:30 +0530

    Sending email with website

--- Send_email.py
+++ Send_email.py
@@ -1,23 +1,14 @@
 import smtplib,ssl
 
 
-host ="smtp.gmail.com"
+def send_mail(message):
+    host = "smtp.gmail.com"
+    post = 465
+    user_name = "XXXXXX@gmail.com"
+    password = "XXXXXXXXX"
+    receiver = "XXXXXXXXXX@gmail.com"
 
-post = 465
-
-user_name = "XXX@gmail.com"
-password = "XXXXXXXXXXXXXXXX"
-
-receiver = "XXXX@gmail.com"
-
-text = """\
-subject: hi 
-how are you
-bye
-"""
-
-context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL(host, post, context=context) as server:
-    server.login(user_name,password)
-    server.sendmail(user_name, receiver, text)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, post, context=context) as server:
+        server.login(user_name, password)
+        server.sendmail(user_name, receiver, message)
--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,4 +1,6 @@
 import streamlit as web
+from Send_email import send_mail
+
 
 
 web.set_page_config()
@@ -6,5 +8,15 @@ web.title("Contact me here:")
 
 with web.form(key="email_form",clear_on_submit=True):
     user_email = web.text_input("your email address")
-    message = web.text_area("please enter your message here")
-    button = web.form_submit_button("submit")
+    raw_message = web.text_area("please enter your message here")
+    message= f"""\
+    subject: email from {user_email}
+    from: {user_email}
+    
+    {raw_message}
+"""
+    button = web.form_submit_button("Send")
+
+    if button:
+        send_mail(message)
+        web.info("your message has been sent!")
